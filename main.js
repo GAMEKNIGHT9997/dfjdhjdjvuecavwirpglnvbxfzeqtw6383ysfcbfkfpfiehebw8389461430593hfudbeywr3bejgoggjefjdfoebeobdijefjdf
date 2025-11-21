44347ca69006a2c8eeaf5b4d2d74d4be328b50f4
GAMEKNIGHT9997 <gameknight9997@gmail.com>
2025-11-21 08:27:42 +0700

    main.js

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-// Show something so the page isn't blank
-document.body.innerHTML = "Loading…";
 
-// Grab the code from the URL
+const card = document.querySelector(".card");
+card.style.opacity = "0";
+card.style.visibility = "hidden";
+
+
 const code = window.location.hash.substring(1);
 
-console.log("Scanned code:", code); // helps you debug
 
-// Check it
 if (code === "XMAS20dfudjie5ehjfdf") {
-	document.body.innerHTML = "Unlocked! Starting…";
-	// your game start here
-	// game.start();
+    console.log("Unlock OK");
+    card.style.opacity = "1";
+    card.style.visibility = "visible";
 } else {
-	document.body.innerHTML = "Scan the QR code to unlock!";
+    console.log("Wrong code — lock");
+    document.body.innerHTML = "<h2>Scan the QR code to unlock!</h2>";
 }
